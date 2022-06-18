70f24745defefc97fcd21bf1c968de9d749b2fd1
Fernando <fernandomercado529@gmail.com>
2022-06-18 13:30:18 -0400

    update component description

--- components/TitleDescription.tsx
+++ components/TitleDescription.tsx
@@ -1,10 +1,10 @@
 
 export const TitleDescription = () => {
   return <>
-          <h1 className='text-3xl font-bold pt-16  capitalize text tracking-wide '>Calculadora de Grasa <br />Corporal</h1>
-          <p className='py-4 text-[#ddd]'>El método de la Marina de Estados Unidos (US Navy Method) ofrece una manera <br />
+          <h1 className='text-3xl font-bold pt-16  capitalize text tracking-wider '>Calculadora de Grasa <br />Corporal</h1>
+          <p className='py-4 text-[#ccc]'>El método de la Marina de Estados Unidos (US Navy Method) ofrece una manera <br />
             sencilla de calcular un aproximado del porcentaje de tejido adiposo en el cuerpo de <br />
             una persona.</p>
-          <p className='py-4 text-[#ddd]'>Los valores requeridos para la fórmula son los siguientes : </p>
+          <p className='py-4 text-[#ccc]'>Los valores requeridos para la fórmula son los siguientes : </p>
   </>
 }
